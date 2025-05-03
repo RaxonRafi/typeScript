cf7dce276b8d24e5f9cd354a8f3a31d137267311
Rafi <itzmeh320@gmail.com>
2025-05-03 22:21:21 +0600

    rest, spread,destructure

--- module1/dist/index.js
+++ module1/dist/index.js
@@ -1,3 +1,12 @@
 "use strict";
 let hello = "Muhammad Rafi hello world!";
 console.log(hello);
+const student = {
+    name: 'muhammad rafi',
+    age: 24,
+    education: {
+        institution: 'Premier University',
+        level: 2
+    }
+};
+console.log(student);
--- module1/src/index.ts
+++ module1/src/index.ts
@@ -1,2 +1,57 @@
+{
+
+
 let hello: string = "Muhammad Rafi hello world!"
 console.log(hello);
+
+type Tobj = {
+    name: string
+    age: number
+    education?:{
+        institution: 'University' | 'College' | 'School'
+        level: 'Undergrad' | 'Graduate'
+    }
+}
+
+const student1:Tobj = {
+    name: 'muhammad rafi',
+    age:24,
+    education:{
+        institution:'University',
+        level: 'Undergrad'
+    }
+}
+
+const student2:Tobj = {
+    name: 'muhammad rafi 2',
+    age:24,
+}
+
+console.log(student1,student2);
+
+const fn = (input: number): number=>{
+    const multi = input*input
+    return multi;
+}
+
+console.log(fn(20));
+
+
+const user = {
+    name: 'Rafi',
+    age: 20
+}
+
+const user2 = {
+    ...user,
+    address: "Chattogram"
+}
+const {name,age} = user2
+
+
+const numbers : number[] = [1,2,3,4,5,6]
+
+const numbers2 : number[] = [7,8,...numbers,10]
+console.log(numbers2);
+
+}
